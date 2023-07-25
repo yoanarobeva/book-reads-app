785aff20517fe2649229e75fceecb6ce44ba0632
yoanarobeva <yoana_robeva@yahoo.com>
2023-07-25 11:12:56 +0300

    profile and friends component inits

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserHomeComponent } from './user-home/user-home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FriendsComponent } from './friends/friends.component';
 
 const routes: Routes = [
     {
         path: '',
         pathMatch: 'full',
         redirectTo: '/home'
-      },
-      {
+    },
+    {
         path: 'home',
         component: UserHomeComponent,
-      },
+    },
+    {
+        path: 'profie',
+        component: ProfileComponent,
+    },
+    {
+        path: 'friends',
+        component: FriendsComponent,
+    },
 ];
 
 @NgModule({
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,11 +4,15 @@ import { MaterialModule } from '../material/material.module';
 import { UserHomeComponent } from './user-home/user-home.component';
 import { ActivityCardComponent } from './activity-card/activity-card.component';
 import { UserRoutingModule } from './user-routing.module';
+import { ProfileComponent } from './profile/profile.component';
+import { FriendsComponent } from './friends/friends.component';
 
 @NgModule({
   declarations: [
     UserHomeComponent,
     ActivityCardComponent,
+    ProfileComponent,
+    FriendsComponent,
   ],
   imports: [
     CommonModule,
